4c8703b82b077e2d5e0ed465b01718877702aad7
José arias <josee.ariasmelo@gmail.com>
2023-03-20 19:31:12 -0300

    Se-inicia-armado-de-las-capas

---
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Transversal.Common/IConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Pacagroup.Ecommerce.Transversal.Common
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection GetConnection { get; }
+    }
+}
