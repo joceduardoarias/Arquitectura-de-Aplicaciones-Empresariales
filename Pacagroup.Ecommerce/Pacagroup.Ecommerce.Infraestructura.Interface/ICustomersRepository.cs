c9f6626ad09feea6596aa304b422aa3414643757
José arias <josee.ariasmelo@gmail.com>
2023-03-20 21:45:46 -0300

    Capa Dominio
    Se agrega entidad Customers y la interfas ICustomers con las operaciones CRUD.

---
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Infraestructura.Interface/ICustomersRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pacagroup.Ecommerce.Domain.Entity;
+
+namespace Pacagroup.Ecommerce.Infraestructura.Interface
+{
+    public interface ICustomersRepository 
+    {
+        #region "Metodos Sincronos"
+        IEnumerable<Customers> GetCustomers();
+        Customers GetCustomer(string customerId);
+        bool InsertCustomer(Customers customer);
+        bool UpdateCustomer(Customers customer);
+        bool DeleteCustomer(string customerId);
+        #endregion
+        
+        #region "Metodos Asincronos"
+        IEnumerable<Customers> GetCustomersAsync();
+        Customers GetCustomerAsync(string customerId);
+        bool InsertCustomerAsync(Customers customer);
+        bool UpdateCustomerAsync(Customers customer);
+        bool DeleteCustomerAsync(string customerId);
+        #endregion
+        
+    }
+}
