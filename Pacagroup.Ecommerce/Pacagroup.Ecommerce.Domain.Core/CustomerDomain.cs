540e3a97dd9b634e4fab14ed25ec605b78056941
José arias <josee.ariasmelo@gmail.com>
2023-03-21 16:44:57 -0300

    Creando la capa de dominio
    Proyecto de interfaz (CRUD), Proyecto Core (lógica y reglas de negocio) en este caso no tenemos reglas de negocio

---
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Domain.Core/CustomerDomain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pacagroup.Ecommerce.Domain.Entity;
+using Pacagroup.Ecommerce.Domain.Interface;
+using Pacagroup.Ecommerce.Infraestructura.Interface;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Ecommerce.Domain.Core
+{
+    public class CustomerDomain : ICustomerDomain
+    {
+        private readonly ICustomersRepository _customersRepository;
+        public CustomerDomain(ICustomersRepository customersRepository)
+        {
+            _customersRepository = customersRepository;
+        }
+        public bool DeleteCustomer(string customerId)
+        {
+            return _customersRepository.DeleteCustomer(customerId);
+        }
+        public async Task<bool> DeleteCustomerAsync(string customerId)
+        {
+            return await _customersRepository.DeleteCustomerAsync(customerId);
+        }
+        public Customers GetCustomer(string customerId)
+        {
+            return _customersRepository.GetCustomer(customerId);
+        }
+        public async Task<Customers> GetCustomerAsync(string customerId)
+        {
+            return await _customersRepository.GetCustomerAsync(customerId);
+        }
+
+        public IEnumerable<Customers> GetCustomers()
+        {
+            return _customersRepository.GetCustomers();
+        }
+
+        public Task<IEnumerable<Customers>> GetCustomersAsync()
+        {
+            return _customersRepository.GetCustomersAsync();
+        }
+
+        public bool InsertCustomer(Customers customer)
+        {
+            return _customersRepository.InsertCustomer(customer);
+        }
+
+        public Task<bool> InsertCustomerAsync(Customers customer)
+        {
+            return _customersRepository.InsertCustomerAsync(customer);
+        }
+
+        public bool UpdateCustomer(Customers customer)
+        {
+            return _customersRepository.UpdateCustomer(customer);
+        }
+
+        public Task<bool> UpdateCustomerAsync(Customers customer)
+        {
+            return _customersRepository.UpdateCustomerAsync(customer);
+        }
+    }
+}
---
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Domain.Interface/ICustomerDomain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pacagroup.Ecommerce.Domain.Entity;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Ecommerce.Domain.Interface
+{
+    public interface ICustomerDomain
+    {
+        #region "Metodos Sincronos"
+        IEnumerable<Customers> GetCustomers();
+        Customers GetCustomer(string customerId);
+        bool InsertCustomer(Customers customer);
+        bool UpdateCustomer(Customers customer);
+        bool DeleteCustomer(string customerId);
+        #endregion
+
+        #region "Metodos Asincronos"
+        Task<IEnumerable<Customers>> GetCustomersAsync();
+        Task<Customers> GetCustomerAsync(string customerId);
+        Task<bool> InsertCustomerAsync(Customers customer);
+        Task<bool> UpdateCustomerAsync(Customers customer);
+        Task<bool> DeleteCustomerAsync(string customerId);
+        #endregion
+    }
+}
