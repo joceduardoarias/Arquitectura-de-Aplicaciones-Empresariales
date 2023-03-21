4a32ec79c1235045c5ac1d6986122956f5697b71
José arias <josee.ariasmelo@gmail.com>
2023-03-21 17:48:03 -0300

    Capa transversal
    Se agrega clase Response que es la clase genérica que se expondrá en la API.

---
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Transversal.Common/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace Pacagroup.Ecommerce.Transversal.Common
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+}
