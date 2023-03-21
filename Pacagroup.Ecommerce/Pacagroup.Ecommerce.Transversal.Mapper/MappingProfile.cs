97ceeda34fcafb92bbcc7577f17428ecbc7c6d79
José arias <josee.ariasmelo@gmail.com>
2023-03-21 17:41:31 -0300

    Capa Transversal
    Agregando funcionalidades comunes configurando Mapper

---
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Transversal.Mapper/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using Pacagroup.Ecommerce.Application.DTO;
+using Pacagroup.Ecommerce.Domain.Entity;
+
+namespace Pacagroup.Ecommerce.Transversal.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customers, CustomersDto>().ReverseMap();            
+        }
+    }
+}
