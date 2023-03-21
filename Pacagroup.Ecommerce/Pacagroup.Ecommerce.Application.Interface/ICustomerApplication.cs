9ec28723d29847b83f5232f081c6b8b604f80c70
José arias <josee.ariasmelo@gmail.com>
2023-03-21 18:01:00 -0300

    Capa Application Interface
    Se agrega la interfaz en la capa de aplicación.

---
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Application.Interface/ICustomerApplication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pacagroup.Ecommerce.Application.DTO;
+using Pacagroup.Ecommerce.Transversal.Common;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Ecommerce.Application.Interface
+{
+    public interface ICustomerApplication
+    {
+        #region "Metodos Sincronos"
+        Response<IEnumerable<CustomersDto>> GetCustomers();
+        Response<CustomersDto> GetCustomer(string customerId);
+        Response<bool> InsertCustomer(CustomersDto customer);
+        Response<bool> UpdateCustomer(CustomersDto customer);
+        Response<bool> DeleteCustomer(string customerId);
+        #endregion
+
+        #region "Metodos Asincronos"
+        Task<Response <IEnumerable<CustomersDto>>> GetCustomersAsync();
+        Task<Response<CustomersDto>> GetCustomerAsync(string customerId);
+        Task<Response<bool>> InsertCustomerAsync(CustomersDto customer);
+        Task<Response<bool>> UpdateCustomerAsync(CustomersDto customer);
+        Task<Response<bool>> DeleteCustomerAsync(string customerId);
+        #endregion
+    }
+}
