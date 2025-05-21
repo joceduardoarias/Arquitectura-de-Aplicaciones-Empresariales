2165ec936e39fb0407be5acb0a9c877584f45928
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-21 16:11:27 -0300

    Agrega telemetría a la API.
    Implementa OpenTelemetry para el seguimiento distribuido de la API.
    Incluye instrumentación para ASP.NET Core y HttpClient.
    Configura el exportador OTLP para enviar datos a un colector.

---
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Services.WebApi/Modules/Instrumentation/InstrumentationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using OpenTelemetry.Trace;
+
+namespace Pacagroup.Ecommerce.Services.WebApi.Modules.Instrumentation
+{
+    public static class InstrumentationExtensions
+    {
+        public static IServiceCollection AddInstrumentation(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddOpenTelemetry()
+            .WithTracing(tracerProviderBuilder =>
+            {
+                tracerProviderBuilder
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()                    
+                    .AddOtlpExporter(options =>
+                    {
+                        options.Endpoint = new Uri(configuration["OTEL_EXPORTER_OTLP_ENDPOINT"] ?? "http://otel-collector:4317");
+                    });
+            });
+
+            return services;
+        }
+    }
+}
